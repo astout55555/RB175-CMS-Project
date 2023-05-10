8ace49eb8cf7b93827addbfb11468f0951fea8bb
astout55555 <alex.the.stout5@gmail.com>
2023-05-09 20:50:11 -0700

    First commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+The final project for RB175 at Launch School. A file-based content management system.
---
+++ cms.rb
@@ -0,0 +1,4 @@
+require "sinatra"
+require "sinatra/reloader" if development?
+require "tilt/erubis"
+require "sinatra/content_for"
