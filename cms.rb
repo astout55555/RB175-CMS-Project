414b565554332aa71b77b628cf33b254871c3d09
astout55555 <alex.the.stout5@gmail.com>
2023-05-24 21:04:58 -0700

    Most of the way through the project now.

--- cms.rb
+++ cms.rb
@@ -2,11 +2,8 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require "tilt/erubis"
 require "sinatra/content_for"
-
-# this ruby idiom finds the absolute path name for this file's parent directory
-root = File.expand_path("..", __FILE__)
-# otherwise we're using relative paths, which are based on where the program
-# is called from--not the location of the file itself!
+require "redcarpet"
+# require "fileutils"
 
 configure do
   enable :sessions
@@ -14,12 +11,49 @@ configure do
   set :erb, :escape_html => true
 end
 
-helpers do
-  
+not_found do
+  status 404
+  erb :not_found
 end
 
-not_found do
-  "Nothing here yo."
+# `__FILE__` refers to the current file (`cms.rb`)
+# method provides absolute path from root to parent of current file (`cms.rb`)
+def data_path
+  if ENV["RACK_ENV"] == "test"
+    File.expand_path("../test/data", __FILE__) # then adds `/test/data`
+  else
+    File.expand_path("../data", __FILE__) # or just `/data`
+  end
+end
+# relative paths are based on where the program is called from...
+# so instead we need absolute paths based on the location of the file itself
+
+def render_markdown(file_text)
+  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+  markdown.render(file_text)
+end
+
+def load_file_content(path)
+  file_contents = File.read(path)
+  case File.extname(path)
+  when ".txt"
+    headers["Content-Type"] = "text/plain"
+    file_contents
+  when ".md"
+    erb render_markdown(file_contents)
+  end
+end
+
+def valid_filename?(filename)
+  filename.match?(/\..*/)
+end
+
+def valid_credentials?(username, password)
+  username == "admin" && password == "secret"
+end
+
+helpers do
+  
 end
 
 before do
@@ -27,13 +61,94 @@ before do
 end
 
 get "/" do
-  @files = Dir.glob(root + "/data/*").map do |path| # pattern matching files
+  pattern = File.join(data_path, "*")
+  @files = Dir.glob(pattern).map do |path| # pattern matching files
     File.basename(path) # returns only the basename, e.g. 'about.txt'
   end
   erb :index
 end
 
+get "/users/signin" do
+  erb :sign_in
+end
+
+post "/users/signin" do
+  if valid_credentials?(params[:username], params[:password])
+    session[:username] = params[:username]
+    session[:message] = "Welcome!"
+    redirect "/"
+  else
+    @username = params[:username]
+    @password = params[:password]
+    session[:message] = "Invalid credentials"
+    status 422
+    erb :sign_in
+  end
+end
+
+post "/users/signout" do
+  session.delete(:username)
+  session[:message] = "You have been signed out."
+  redirect "/"
+end
+
+get "/new" do
+  erb :new
+end
+
+post "/new" do
+  if valid_filename?(params[:filename])
+    file_path = File.join(data_path, params[:filename])
+    File.open(file_path, 'w') do |file|
+      file.write(params[:contents])
+    end
+  
+    session[:message] = "#{params[:filename]} has been created."
+    redirect "/"
+  else
+    session[:message] = "A filename and extension are required."
+    redirect "/new"
+  end
+end
+
 get "/:filename" do
-  headers["Content-Type"] = "text/plain"
-  File.read(root + "/data/" + params[:filename])
+  file_path = File.join(data_path, params[:filename])
+
+  if File.file?(file_path)
+    load_file_content(file_path)
+  else
+    session[:message] = "#{params[:filename]} does not exist."
+    redirect "/"
+  end
+end
+
+get "/:filename/edit" do
+  file_path = File.join(data_path, params[:filename])
+
+  if File.file?(file_path)
+    @file_name = File.basename(file_path)
+    @content = File.read(file_path)
+    erb :edit
+  else
+    session[:message] = "#{params[:filename]} does not exist."
+    redirect "/"
+  end
+end
+
+post "/:filename/edit" do
+  file_path = File.join(data_path, params[:filename])
+  File.open(file_path, 'w') do |file|
+    file.write(params[:contents])
+  end
+
+  session[:message] = "#{params[:filename]} has been updated."
+  redirect "/"
+end
+
+post "/:filename/delete" do
+  filepath = File.join(data_path, params[:filename])
+  File.delete(filepath)
+
+  session[:message] = "#{params[:filename]} has been deleted."
+  redirect "/"
 end
--- test/cms_test.rb
+++ test/cms_test.rb
@@ -1,5 +1,7 @@
 ENV["RACK_ENV"] = "test"
 
+require "fileutils"
+
 require "minitest/autorun"
 require "rack/test"
 require "minitest/reporters"
@@ -10,8 +12,14 @@ require_relative "../cms"
 class CmsTest < Minitest::Test
   include Rack::Test::Methods
 
+  def create_document(name, content = "")
+    File.open(File.join(data_path, name), "w") do |file|
+      file.write(content)
+    end
+  end
+
   def setup
-    @root = File.expand_path("../..", __FILE__) # project root dir
+    FileUtils.mkdir_p(data_path)
   end
 
   def app
@@ -19,28 +27,179 @@ class CmsTest < Minitest::Test
   end
 
   def test_index
+    create_document "about.md"
+    create_document "changes.txt"
+    create_document "history.txt"
+
     get "/"
     assert_equal 200, last_response.status
     assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
-    assert_includes last_response.body, "/about.txt"
+    assert_includes last_response.body, "/about.md"
     assert_includes last_response.body, "/changes.txt"
     assert_includes last_response.body, "/history.txt"
   end
 
-  def test_viewing_text_documents
-    get "/about.txt"
+  def test_sign_in_page
+    get "/users/signin"
     assert_equal 200, last_response.status
-    assert_equal "text/plain", last_response["Content-Type"]
-    assert_includes last_response.body, "are an extremely important animal for medical"
+    assert_includes last_response.body, "<form action=\"/users/signin\""
+  end
 
-    get "/changes.txt"
+  def test_sign_in_valid
+    post "/users/signin", username: "admin", password: "secret"
+    assert_equal 302, last_response.status
+    get last_response["Location"]
     assert_equal 200, last_response.status
-    assert_equal "text/plain", last_response["Content-Type"]
-    assert_includes last_response.body, "They got a war on drugs so the police can bother me"
+    assert_includes last_response.body, "Welcome!"
+
+    get "/"
+    assert_includes last_response.body, "Signed in as"
+    refute_includes last_response.body, "Welcome!"
+  end
+
+  def test_invalid_sign_in
+    post "/users/signin", username: "test", password: "test"
+    assert_equal 422, last_response.status
+    assert_includes last_response.body, "Invalid credentials"
+    assert_includes last_response.body, "<form action=\"/users/signin\""
+  end
+
+  def test_sign_out
+    post "/users/signout"
+    assert_equal 302, last_response.status
+    
+    get last_response["Location"]
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "You have been signed out."
+    assert_includes last_response.body, "<a class=\"user_status\" href=\"/users/signin\">"
+
+    get "/"
+    refute_includes last_response.body, "You have been signed out."
+  end
+
+  def test_viewing_text_documents
+    create_document "history.txt", "2019 - Ruby 2.7 released."
 
     get "/history.txt"
     assert_equal 200, last_response.status
     assert_equal "text/plain", last_response["Content-Type"]
     assert_includes last_response.body, "2019 - Ruby 2.7 released."
+  end
+
+  def test_viewing_markup_doc
+    create_document "about.md", "# Ruby is..."
+
+    get "/about.md"
+    assert_equal 200, last_response.status
+    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+    assert_includes last_response.body, "<h1>Ruby is...</h1>"
+  end
+
+  def test_nonexistant_file_error
+    get "/notafile.ext"
+    assert_equal 302, last_response.status
+
+    get last_response["Location"] # Request the page that the user was redirected to
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "notafile.ext does not exist."
+
+    get "/"
+    refute_includes last_response.body, "notafile.ext does not exist."
+  end
+
+  def test_file_edit_form
+    create_document "changes.txt"
+
+    get "/changes.txt/edit"
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "Edit content of changes.txt"
+    assert_includes last_response.body, "<form action=\"/changes.txt/edit\" method=\"post\">"
+  end
+
+  def test_updating_document
+    post "/changes.txt/edit", contents: "new content" # pass in param key/value
+    assert_equal 302, last_response.status
+    get last_response["Location"]
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "changes.txt has been updated."
+
+    get "/"
+    refute_includes last_response.body, "changes.txt has been updated."
+
+    get "/changes.txt"
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "new content"
+  end
+
+  def test_new_doc_form
+    get "/new"
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "<button type=\"submit\">Create"
+  end
+
+  def test_create_document
+    post "/new", filename: "test_file.md"
+    assert_equal 302, last_response.status
+    get last_response["Location"]
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "test_file.md has been created."
+
+    get "/"
+    assert_includes last_response.body, "test_file.md"
+    refute_includes last_response.body, "test_file.md has been created."
+
+    get "/test_file.md"
+    assert_equal 200, last_response.status
+  end
+
+  def test_new_doc_validation
+    post "/new", filename: "test_file"
+    assert_equal 302, last_response.status
+    get last_response["Location"]
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "A filename and extension are required."
+    assert_includes last_response.body, "<button type=\"submit\">Create"
+
+    get "/"
+    refute_includes last_response.body, "test_file"
+
+    get "/test_file"
+    assert_equal 302, last_response.status
+    get last_response["Location"]
+    assert_equal 200, last_response.status
+    assert_includes  last_response.body, "test_file does not exist"
+
+    post "/new", filename: ""
+    assert_equal 302, last_response.status
+    get last_response["Location"]
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "A filename and extension are required."
+    assert_includes last_response.body, "<button type=\"submit\">Create"
+  end
+
+  def test_delete_file
+    create_document "test_file.txt"
+
+    get "/"
+    assert_includes last_response.body, "test_file.txt"
+
+    post "test_file.txt/delete"
+    assert_equal 302, last_response.status
+
+    get last_response["Location"]
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "test_file.txt has been deleted."
+
+    get "/"
+    refute_includes last_response.body, "test_file.txt"
+  end
+
+  def test_not_found
+    get "somethingthat/does/not/exist"
+    assert_equal 404, last_response.status
+  end
+
+  def teardown
+    FileUtils.rm_rf(data_path)
   end
 end
